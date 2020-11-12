c392ba735b15ae20dac05fdfdf408846a212757c
PCBakerman <72618089+PCBakerman@users.noreply.github.com>
2020-11-12 13:03:59 -0600

    Initial commit. Created SuperHero model, initial migration.

---
+++ Superhero Creator/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Superhero_Creator.Models;
+
+namespace Superhero_Creator.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        // add a property of type DbSet<>
+        public DbSet<SuperHeroEntry> SuperHeroEntries { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Superhero Creator/Models/FirstModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Superhero_Creator.Models
+{
+    //this model will represent a table in your database and the properties represent the columns of that table in the database.
+    
+    public class SuperHeroEntry
+    {   [Key]
+        public int ID { get; set; }
+        public string SuperName { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimaryAbility { get; set; }
+        public string SecondaryAbility { get; set; }
+        public string Catchphrase { get; set; }
+    }
+}
